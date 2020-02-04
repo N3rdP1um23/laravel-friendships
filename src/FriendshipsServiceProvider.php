983f2edf4d0e1f18ae1795d9c0e3b4796e67b3e3
Ian Torres <iantorres@outlook.com>
2020-02-04 04:56:58 -0300

    Service provider fixed.

--- src/FriendshipsServiceProvider.php
+++ src/FriendshipsServiceProvider.php
@@ -18,16 +18,16 @@ class FriendshipsServiceProvider extends ServiceProvider
             return;
         }
 
-        $stub      = __DIR__ . '/database/migrations/';
+        $stub      = realpath(dirname(__DIR__).'/tests/database/migrations/') . '/';
         $target    = database_path('migrations') . '/';
 
         $this->publishes([
-            $stub . 'create_friendships_table.php'        => $target . date('Y_m_d_His', time()) . '_create_friendships_table.php',
-            $stub . 'create_friendships_groups_table.php' => $target . date('Y_m_d_His', time() + 1) . '_create_friendships_groups_table.php'
+            $stub . '0000_00_00_000000_create_friendships_table.php'        => $target . date('Y_m_d_His', time()) . '_create_friendships_table.php',
+            $stub . '0000_00_00_000000_create_friendships_groups_table.php' => $target . date('Y_m_d_His', time() + 1) . '_create_friendships_groups_table.php'
         ], 'migrations');
 
         $this->publishes([
-            __DIR__ . '/config/friendships.php' => config_path('friendships.php'),
+            __DIR__ . '/../tests/config/friendships.php' => config_path('friendships.php'),
         ], 'config');
 
     }
